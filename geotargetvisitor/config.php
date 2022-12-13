db1c767fcf9e0fb033f1b79ac19404b7ee126b01
ipol88 <94164952+ipol88@users.noreply.github.com>
2022-12-14 02:54:04 +0700

    Add files via upload

---
+++ geotargetvisitor/config.php
@@ -0,0 +1,10 @@
+<?php
+
+// untuk menambahkan tinggal copy yang ada terus paste di bawahnya, 
+// ganti kode negara sesuai countrycode.org
+
+$linkOffer = array( 
+    "ALL" => "https://www.detik.com", // Yang ini jangan dihapus, sebagai default
+    "US" => "https://www.cnn.com",
+    "SG" => "https://www.mtvasia.com",
+);
